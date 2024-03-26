cbf87e488c085cb6a985a972b90ff7812c06d070
Natnael-Alemayehu <61546827+Natnael-Alemayehu@users.noreply.github.com>
2024-03-26 15:36:17 +0300

    Tags table aggregation finished.

--- main.py
+++ main.py
@@ -6,15 +6,25 @@ from dotenv import load_dotenv
 load_dotenv()
 
 print("Writing all the account ID's")
-import write_all_account
+# import write_all_account
 print("Writing all the account ID's that are active")
-import write_to_accountid_active
+# import write_to_accountid_active
 print("Writing all the account ID's that are expired")
-import write_to_accountid_expired
+# import write_to_accountid_expired
 print("Writing account Id's is complete")
+print("****************************************")
+print("Aggregating the accountid_name table STARTED")
 # import accountid_name
+print("Aggregating the accountid_name table FINISHED")
+print("****************************************")
+
+print("Aggregating the Leads table STARTED")
 # import leads
+print("Aggregating the Leads table FINISHED")
+print("****************************************")
+print("Aggregating the tags table STARTED")
 # import tags
+print("Aggregating the tags table FINISHED")
 # import clients
 # import teams
 # import campaign
