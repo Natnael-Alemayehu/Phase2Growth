864496dc56d7e9f34cff4d64637c4b88d9dddd2a
Natnael-Alemayehu <61546827+Natnael-Alemayehu@users.noreply.github.com>
2024-04-12 13:10:10 +0300

    Deployment integration - Airtable connect. V5

--- app.py
+++ app.py
@@ -1,12 +1,13 @@
 from flask import Flask
 app = Flask(__name__)
+app.debug = True
 
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
 
 @app.route('/airtable')
-def hello_world():
+def testing_path():
     message = "This is from the airtable route!"
     import deployment_test
     return message
--- deployment_test.py
+++ deployment_test.py
@@ -8,17 +8,18 @@ load_dotenv()
 #Connecting to airtable
 
 # Use string literals for environment variable names
-AIRTABLE_BASE_ID = os.environ.get("AIRTABLE_BASE_ID")
-AIRTABLE_API_KEY = os.environ.get("AIRTABLE_API_KEY")
+# AIRTABLE_BASE_ID = os.environ.get("AIRTABLE_BASE_ID")
+# AIRTABLE_API_KEY = os.environ.get("AIRTABLE_API_KEY")
 
-AIRTABLE_BASE_ID = os.getenv("AIRTABLE_BASE_ID")
 AIRTABLE_API_KEY = os.getenv("AIRTABLE_API_KEY")
 AIRTABLE_TABLE_NAME = 'test_table'
+
+new_api_key = "patKmC8C1pURk5AoN.177615f8e005685a3bcfecb8b1ad02f58395dda1c1d2ee1787ae438cf2778d25"
 
 endpoint = f'https://api.airtable.com/v0/apphrxDtb300tgxfL/{AIRTABLE_TABLE_NAME}'
 #Python request headers 
 headers = {
-    "Authorization": f"Bearer {AIRTABLE_API_KEY}",
+    "Authorization": f"Bearer {new_api_key}",
     "Content-Type" : "application/json"
 }
 
